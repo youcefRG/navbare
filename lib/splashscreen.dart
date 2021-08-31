9a53f70e6793770373bcb3e415e39044635a481f
Youcef <youyou.si@outlook.fr>
2021-08-31 09:38:35 +0200

    animation succeed

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:navbare/home.dart';
+import 'package:rive/rive.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -12,17 +13,29 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+ // dynamic _opacity =0.0;
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 10), (){
+    //animation();
+    /*Timer(Duration(seconds: 5), (){
       Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_)=> Home()
       ));
-    });
+    }); */
+
 
 
   }
+/*void animation (){
+  Future.delayed(Duration(milliseconds: 2000), () {
+    setState(() {
+      _opacity=1.0;
 
+    });
+    // Do something
+  });
+}*/
   
   @override
   Widget build(BuildContext context) {
@@ -30,17 +43,41 @@ class _SplashScreenState extends State<SplashScreen> {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('lottie animation'),
-        ),
 
-        body: Center(
-          child: Container(
-            child: Lottie.asset('assets/animation.json'),
 
-          )
+        body: Stack(
+          fit: StackFit.loose,
 
-        ),
+           children: <Widget>[
+
+             RiveAnimation.asset('assets/realintroanim.riv',fit: BoxFit.fill),
+
+
+
+            Positioned(
+              bottom:90,
+              left: 74,
+
+              child:Opacity(
+                opacity: 0.0,
+
+                child:new InkWell(
+                  splashColor: Colors.blue,
+                  onTap: (){
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_)=>SplashScreen())
+                    );
+                  },
+                  child:Image.asset('assets/button.png',width: 210,height: 60,),
+                ) ,
+              ),
+
+
+            )
+
+
+          ]
+          ),
+
 
       ),
     );
